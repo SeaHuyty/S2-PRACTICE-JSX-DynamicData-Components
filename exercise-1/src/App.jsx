742f8aeab6e4717a9b482629420f23070619087a
SeaHuyty <seahuyty4444@gmail.com>
2025-02-18 20:55:35 +0700

    Final Commit

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
+function Header() {
+  return (
+      <>
+          <header className="block">
+          <h2>Welcome to this course!</h2>
+          </header>
+      </>
+  )
+}
+
 function App() {
   return (
     <>
+      
+      <Header />
+
       <main className="block">
         <p>
           React (also known as React.js or ReactJS) is a free and open-source
